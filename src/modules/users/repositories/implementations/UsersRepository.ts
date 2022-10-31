da136f8556db2cb33497832e717a0518b2fead67
Filipi Dalfovo <filipi.dalfovo@gmail.com>
2022-10-31 10:50:53 -0300

    final

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -11,20 +11,19 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
+  async findUserWithGamesById({ user_id }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    return await this.repository.findOneOrFail(user_id, { relations: ['games']});
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE LOWER(first_name) = LOWER('${first_name}') and LOWER(last_name) = LOWER('${last_name}')`);
   }
 }
